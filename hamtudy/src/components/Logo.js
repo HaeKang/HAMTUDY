fccd0373e330f09baad6c933a91dc920d91ab216
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-07-25 04:10:01 +0900

    test code

--- hamtudy/src/components/Logo.js
+++ hamtudy/src/components/Logo.js
@@ -1,10 +1,11 @@
 import React from "react";
 import styled from "styled-components";
+import colors from "../styles/colors";
 function Logo() {
   return <LogoWrapper>Hamtudy</LogoWrapper>;
 }
 const LogoWrapper = styled.div`
-  color: #46ad78;
+  color: ${colors.theme};
   font-weight: 800;
   font-size: 30px;
 `;
