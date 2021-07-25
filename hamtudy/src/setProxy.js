7aa20ec8d2e5f7f3402706698a4d9aa05a578f2f
root <root@ip-172-31-20-76.us-east-2.compute.internal>
2021-07-25 12:15:57 +0000

    하아아아아아

--- hamtudy/src/setProxy.js
+++ hamtudy/src/setProxy.js
@@ -2,6 +2,8 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app){
     app.use(
-        proxy()
-    )
-}
+        proxy('/api', {
+		target : 'http://3.142.49.52:8080/'
+	})
+    );
+};
