93f55114376278cb11b74e1e128d636df5f88b5c
haekang <ydjm1027@naver.com>
2021-08-01 13:45:34 +0900

    test

--- hamtudy/server/routes/index.js
+++ hamtudy/server/routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req,res){
-	res.send("hello");
+	res.json([{
+        'id' : "testing_api",
+        'name' : '방가방가햄토리'
+    }]);
 });
 
 module.exports = router;
--- hamtudy/server/server.js
+++ hamtudy/server/server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const app = express();
 const cors = require('cors');
-const api = require('./routes/index.js');
+const port = 8080;
 
 app.use(cors());
-app.use('/', api);
 
-const port = 8080;
 
 app.listen(port, function(){
     console.log(`서버가 ${port}번 포트에서 실행중 입니다.`);
 });
 
 
-app.post("/test_api", function(req,res){
-    res.send([{
-        'id' : "testing_api",
-        'name' : '방가방가햄토리'
-    }]);
+app.get('/', function(req,res){
+	res.send("hello");
 });
+
+
+var testRouter = require('./routes/index.js');
+app.use('/test_api', testRouter);
