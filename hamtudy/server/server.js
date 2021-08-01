95e965e0724a931c6d5ee57c1409aa18a1fde5fd
haekang <ydjm1027@naver.com>
2021-08-01 13:22:17 +0900

    Update server.js

--- hamtudy/server/server.js
+++ hamtudy/server/server.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const api = require('./routes/index.js');
 
 app.use(cors());
-app.use('/api', api);
+app.use('/', api);
 
 const port = 8080;
 
@@ -12,3 +12,10 @@ app.listen(port, function(){
     console.log(`서버가 ${port}번 포트에서 실행중 입니다.`);
 });
 
+
+app.post("/test_api", function(req,res){
+    res.send([{
+        'id' : "testing_api",
+        'name' : '방가방가햄토리'
+    }]);
+});
