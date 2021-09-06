51f5328b61faf6810ec274f0a5f4f1abe2d8567c
dndbekfrl1 <dndbekfrl1@gmail.com>
2021-09-06 14:25:53 +0900

    Update SignUp.js

--- hamtudy/src/components/SignUp.js
+++ hamtudy/src/components/SignUp.js
@@ -1,9 +1,10 @@
-import react, { useState } from "react";
+import react, { useRef, useState } from "react";
 import SignUpTemplate from "./SignUpTemplate";
 import styled from "styled-components";
 import axios from "axios";
 import { useDispatch } from "react-redux";
 import { signUp } from "../modules/userService";
+import colors from "../styles/colors";
 //TODO 회원가입
 /**
  * 아이디 중복검사
@@ -13,73 +14,110 @@ import { signUp } from "../modules/userService";
  */
 function SignUp() {
   const dispatch = useDispatch();
-  const [inputs, setInputs] = useState({
-    user_id: "",
-    user_pw: "",
-    user_nick: "",
-  });
-  const onChange = (e) => {
-    const { value, name } = e.target;
-    setInputs({
-      ...inputs,
-      [name]: value,
-    });
-  };
 
+  const [user_id, setUserId] = useState(null);
+  const [user_pw, setUserPw] = useState(null);
+  const [check_user_pw, setUserCheckPw] = useState(null);
+  const [user_nick, setUserNick] = useState(null);
+
+  const onChangeId = (e) => {
+    setUserId(e.target.value);
+  };
+  const onChangePw = (e) => {
+    setUserPw(e.target.value);
+  };
+  const onChangeCheckPw = (e) => {
+    setUserCheckPw(e.target.value);
+  };
+  const onChangeNick = (e) => {
+    setUserNick(e.target.value);
+  };
   const onClick = (e) => {
     e.preventDefault();
-    dispatch(signUp(inputs))
-      .then((res) => {
-        console.log("ㄱㄷㄴ", res);
-      })
-      .catch((e) => console.log("e", e));
+    if (!isFufilled()) {
+      console.log(user_id, user_pw, user_nick);
+      window.alert("채워주세요");
+    } else {
+      dispatch(
+        signUp({ user_id: user_id, user_pw: user_pw, user_nick: user_nick })
+      )
+        .then((res) => {
+          console.log("ㄱㄷㄴ", res);
+        })
+        .catch((e) => console.log("e", e));
+    }
+  };
+
+  const isFufilled = () => {
+    if (user_id === null || user_pw === null || user_nick === null) {
+      return false;
+    }
+    return true;
   };
 
   return (
     <SignUpTemplate>
-      <>
+      <FromWrapper>
         <div>
           <h2>회원가입</h2>
           <div>
-            <Input
-              onChange={onChange}
-              name="user_id"
-              type="text"
-              placeholder="아이디"
-            />
+            <div className="label">아이디</div>
+            <Input onChange={onChangeId} name="user_id" type="text" />
           </div>
           <div>
+            <div className="label">비밀번호</div>
+            <Input onChange={onChangePw} name="user_pw" type="password" />
+          </div>
+          <div>
+            <div className="label">비밀번호 재확인</div>
             <Input
-              onChange={onChange}
-              name="user_pw"
+              onChange={onChangeCheckPw}
+              name="check_user_pw"
               type="password"
-              placeholder="비밀번호"
             />
           </div>
           <div>
-            <Input name="pwd" type="password" placeholder="비밀번호 확인" />
+            <div className="label">닉네임</div>
+            <Input onChange={onChangeNick} name="user_nick" type="text" />
           </div>
-          <div>
-            <Input
-              onChange={onChange}
-              name="user_nick"
-              type="text"
-              placeholder="닉네임"
-            />
-          </div>
-          <button onClick={onClick}>회원가입</button>
+          <Button className="qulified" onClick={onClick}>
+            회원가입
+          </Button>
         </div>
-      </>
+      </FromWrapper>
     </SignUpTemplate>
   );
 }
 
 const FromWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
+  margin: 10px 0;
+  h2 {
+    margin: 20px 0;
+  }
+  .label {
+    font-size: 12px;
+    color: ${colors.darkgray};
+  }
 `;
 const Input = styled.input`
-  border-bottom: 1px solid black;
+  background-color: ${colors.gray};
+  min-width: 300px;
+  min-height: 40px;
+  border-radius: 4px;
+  padding: 8px 12px;
+  & {
+    margin-bottom: 20px;
+  }
+`;
+const Button = styled.button`
+  min-width: 300px;
+  min-height: 40px;
+  border-radius: 4px;
+  padding: 8px 12px;
+  background-color: ${colors.gray};
+  &.quilfied {
+    background-color: black;
+  }
 `;
 
 export default SignUp;
